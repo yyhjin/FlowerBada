f6f8b9f568081943c080e9991db5a9aa64080cbf
Ssoco <dlscjs8646@gmail.com>
2022-10-20 11:26:09 +0900

    :wrench: swagger 설정 변경

--- backend/src/main/java/app/bada/flower/config/SwaggerConfig.java
+++ backend/src/main/java/app/bada/flower/config/SwaggerConfig.java
@@ -2,32 +2,37 @@ package app.bada.flower.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
+@EnableWebMvc
 public class SwaggerConfig {
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
                 .useDefaultResponseMessages(false)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("app.bada.flower.controller"))
+                .apis(RequestHandlerSelectors.basePackage("app.bada.flower.api.controller"))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo());
+                .enable(true);
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Swagger Test")
-                .description("SwaggerConfig")
-                .version("3.0")
+                .title("꽃바다 API")
+                .description("꽃바다 관련 API 테스트 상세소개 및 사용법")
+                .version("0.0.1")
+                .contact(new Contact("flowerbada", "flowerbada.app", "wendy0301666@gmail.com"))
                 .build();
     }
 
