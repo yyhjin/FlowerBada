52842cc803e4732c3ddd7e6ef3d80e9fa7b672a2
Ssoco <dlscjs8646@gmail.com>
2022-10-20 11:25:26 +0900

    :wrench: 서비스 인터페이스 추가

--- backend/src/main/java/app/bada/flower/api/service/UserService.java
+++ backend/src/main/java/app/bada/flower/api/service/UserService.java
@@ -1,19 +1,7 @@
 package app.bada.flower.api.service;
 
 import app.bada.flower.api.dto.user.SignInResDto;
-import app.bada.flower.api.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
-public class UserService {
-    private final UserRepository userRepository;
-
-    public SignInResDto signIn(String token) {
-        // 로그인 로직 만들고 수정 필요
-        userRepository.findById(123);
-        SignInResDto tmp = new SignInResDto();
-        return tmp;
-    }
+public interface UserService {
+    SignInResDto signIn(String token);
 }
---
+++ backend/src/main/java/app/bada/flower/api/service/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package app.bada.flower.api.service;
+
+import app.bada.flower.api.dto.user.SignInResDto;
+import app.bada.flower.api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService{
+    private final UserRepository userRepository;
+
+    public SignInResDto signIn(String token) {
+        // 로그인 로직 만들고 수정 필요
+        userRepository.findById(123);
+        SignInResDto tmp = new SignInResDto();
+        return tmp;
+    }
+}
