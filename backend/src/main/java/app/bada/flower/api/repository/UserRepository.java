f4a3679dedd5c230a2e333205b8f51562043db74
Ssoco <dlscjs8646@gmail.com>
2022-10-20 11:25:42 +0900

    :bug: Repository annotation 추가

--- backend/src/main/java/app/bada/flower/api/repository/UserRepository.java
+++ backend/src/main/java/app/bada/flower/api/repository/UserRepository.java
@@ -2,6 +2,8 @@ package app.bada.flower.api.repository;
 
 import app.bada.flower.api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 }
