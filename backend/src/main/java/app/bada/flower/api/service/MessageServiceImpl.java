0101820a8064297065ddf662d27f109004da192b
yoonhyejin <wendy0301@naver.com>
2022-10-21 11:38:00 +0900

    :wrench: Repository 관련 주석 해제

--- backend/src/main/java/app/bada/flower/api/service/MessageServiceImpl.java
+++ backend/src/main/java/app/bada/flower/api/service/MessageServiceImpl.java
@@ -4,6 +4,7 @@ import app.bada.flower.api.dto.message.MessageReqDto;
 import app.bada.flower.api.entity.Message;
 import app.bada.flower.api.repository.FlowerItemRepository;
 import app.bada.flower.api.repository.MessageRepository;
+import app.bada.flower.api.repository.RollingPaperRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +13,12 @@ import org.springframework.stereotype.Service;
 public class MessageServiceImpl implements MessageService{
     private final MessageRepository messageRepository;
     private final FlowerItemRepository flowerItemRepository;
-    //private final RollingPaperRepository rollingPaperRepository;
+    private final RollingPaperRepository rollingPaperRepository;
 
     public Message createMessage(MessageReqDto messageReqDto) {
 
         Message message = Message.builder()
-                //.rollingPaper(rollingPaperRepository.getReferenceById(messageReqDto.getRollingId()))
+                .rollingPaper(rollingPaperRepository.getReferenceById(messageReqDto.getRollingId()))
                 .flowerItem(flowerItemRepository.getReferenceById(messageReqDto.getFlowerId()))
                 .content(messageReqDto.getContent())
                 .writer(messageReqDto.getWriter())
