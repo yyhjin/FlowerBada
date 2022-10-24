46ea22966eb821998681e161a6e8b8fce9d909bf
Ssoco <dlscjs8646@naver.com>
2022-10-24 15:52:21 +0900

    Feat be/basic payment

---
+++ backend/src/main/java/app/bada/flower/api/service/PaymentServiceImpl.java
@@ -0,0 +1,10 @@
+package app.bada.flower.api.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentServiceImpl {
+
+}
