423c80e4afa93e70d393b82cb8f6b426fc25aa42
hongjoo <dlghdwn10@naver.com>
2022-10-26 01:07:37 +0900

    :wrench: paymentService implements 추가

--- backend/src/main/java/app/bada/flower/api/service/PaymentServiceImpl.java
+++ backend/src/main/java/app/bada/flower/api/service/PaymentServiceImpl.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class PaymentServiceImpl {
+public class PaymentServiceImpl implements PaymentService{
 
 }
