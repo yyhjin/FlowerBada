2fbfe79ecb7a9090c11ffd3c3b3b0a33ca701a68
hongjoo <dlghdwn10@naver.com>
2022-10-26 06:00:51 +0900

    :wrench: security swagger 설정 변경중

--- backend/src/main/java/app/bada/flower/api/controller/MyPageController.java
+++ backend/src/main/java/app/bada/flower/api/controller/MyPageController.java
@@ -6,6 +6,7 @@ import app.bada.flower.api.dto.mypage.DeliveryResDto;
 import app.bada.flower.api.dto.mypage.MyPointResDto;
 import app.bada.flower.api.service.DeliveryService;
 import app.bada.flower.api.service.MyPageService;
+import app.bada.flower.api.service.jwt.JwtTokenUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @Api(value = "마이페이지 API", tags = {"마이페이지"})
@@ -30,34 +32,29 @@ public class MyPageController {
     @Autowired
     private final MyPageService myPageService;
 
+    @Autowired
+    private final JwtTokenUtil jwtTokenUtil;
     @GetMapping("/delivery")
     @ApiOperation(value="배송중 목록", notes="sort = 1(최신순), 2(오래된순)")
     public ResponseEntity deliveringList(@RequestHeader(value = "X-AUTH-TOKEN", required = false) String jwtToken, @RequestParam int sort, @RequestParam int paginationId) {
         PageRequest pageRequest = PageRequest.of(paginationId,10);
 
-        // 후에 수정
-        int userId  = 1;
-        if (userId != 0) {
-            List<DeliveryResDto> response = deliveryService.selectAllDelivery(userId,sort,pageRequest);
-            return new ResponseEntity(new ResponseDto(response), HttpStatus.OK);
-        }
-        else {
-            return new ResponseEntity(new ResponseDto("로그인이 필요한 서비스입니다."), HttpStatus.FORBIDDEN);
-        }
+        List<DeliveryResDto> response = deliveryService.selectAllDelivery(jwtTokenUtil.getUserId(jwtToken),sort,pageRequest);
+        return new ResponseEntity(new ResponseDto(response), HttpStatus.OK);
+
     }
     @GetMapping("/mypoint")
     @ApiOperation(value="나의 포인트 목록", notes="최신순 나열")
-    public ResponseEntity myPointList(@RequestHeader(value = "X-AUTH-TOKEN", required = false) String jwtToken, @RequestParam int paginationId) {
-        PageRequest pageRequest = PageRequest.of(paginationId,10);
+    public ResponseEntity myPointList(Principal principal, @RequestParam int paginationId) {
+//        System.out.println(jwtTokenUtil.getUserId(jwtToken));
+        if(principal!=null){
+            System.out.println(principal.getName());
+            PageRequest pageRequest = PageRequest.of(paginationId,10);
 
-        // 후에 수정
-        int userId  = 1;
-        if (userId != 0) {
-            MyPointResDto response = myPageService.selectAllMyPoint(userId,pageRequest);
-            return new ResponseEntity(new ResponseDto(response), HttpStatus.OK);
-        }
-        else {
-            return new ResponseEntity(new ResponseDto("로그인이 필요한 서비스입니다."), HttpStatus.FORBIDDEN);
+            MyPointResDto response = myPageService.selectAllMyPoint(Integer.parseInt(principal.getName()),pageRequest);
+            return new ResponseEntity(new ResponseDto(), HttpStatus.OK);
+        }else{
+            return new ResponseEntity(new ResponseDto(),HttpStatus.BAD_REQUEST);
         }
     }
 }
