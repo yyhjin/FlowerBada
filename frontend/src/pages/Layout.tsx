bd8a4bc1a1acade5f73caebc8abfe23da6af3d1c
hongjoo <dlghdwn10@naver.com>
2022-11-01 04:36:24 +0900

    :lipstick: outlet margin 주기

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,11 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import Header from '@components/header/Header';
-
+import { css } from '@emotion/react';
 export default function Layout() {
   return (
     <>
       <Header />
-      <Outlet />
+      <div css={OutletCss}>
+        <Outlet />
+      </div>
     </>
   );
 }
+
+const OutletCss = css`
+  margin-top: 60px;
+`;
