3242c706dcb1e2a58449cf9929ebe819d5cb45da
오석호 <dlscjs8646@naver.com>
2022-10-31 15:07:55 +0900

    :wrench: emotion 적용

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import eslint from 'vite-plugin-eslint';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), { ...eslint(), enforce: 'pre' }],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+    tsconfigPaths(),
+    { ...eslint(), enforce: 'pre' },
+  ],
 });
