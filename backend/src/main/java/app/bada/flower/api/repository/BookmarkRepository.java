9b46b203dc5cf73da152b9c1fd8d8ecc2f369496
배윤호 <bem2183@naver.com>
2022-10-31 10:56:15 +0900

    :sparkles: 그린하우스 내가 작성한 롤페, 즐찾 롤페 페이지네이션 및 정렬 필터 추가

--- backend/src/main/java/app/bada/flower/api/repository/BookmarkRepository.java
+++ backend/src/main/java/app/bada/flower/api/repository/BookmarkRepository.java
@@ -3,6 +3,8 @@ package app.bada.flower.api.repository;
 import app.bada.flower.api.entity.Bookmark;
 import app.bada.flower.api.entity.RollingPaper;
 import app.bada.flower.api.entity.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,5 +15,8 @@ import java.util.Optional;
 @Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark, Integer> {
     List<Bookmark> findAllByUserAndIsValid(User user, boolean check);
+
+    Optional<Slice<Bookmark>> findAllByUserAndIsValidAndIsDeletedFalseOrderByCreatedDateDesc(User user,boolean check, Pageable pageable);
+    Optional<Slice<Bookmark>> findAllByUserAndIsValidAndIsDeletedFalseOrderByCreatedDate(User user,boolean check, Pageable pageable);
     Bookmark findByRollingPaper_IdAndUser_Id(int rollingId, int userId);
 }
--- backend/src/main/java/app/bada/flower/api/service/GreenhouseService.java
+++ backend/src/main/java/app/bada/flower/api/service/GreenhouseService.java
@@ -2,12 +2,13 @@ package app.bada.flower.api.service;
 
 import app.bada.flower.api.dto.greenhouse.GreenhouseResDto;
 import app.bada.flower.api.entity.RollingPaper;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface GreenhouseService {
 
-    List<GreenhouseResDto> getMyRollingPapers(String token);
+    List<GreenhouseResDto> getMyRollingPapers(String token,int sortNumber, Pageable pageable);
 
-    List<GreenhouseResDto> getMyBookmarks(String token);
+    List<GreenhouseResDto> getMyBookmarks(String token,int sortNumber, Pageable pageable);
 }
