f6a75e87cca603d7bdee8c8ee3942bb7df4109eb
Ssoco <dlscjs8646@naver.com>
2022-11-01 14:12:41 +0900

    [FEATURE] 전체 CSS 작업을 위한 기본 메인페이지 틀 생성

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,17 +1,27 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
-import Header from '../components/header/Header';
-import { userReCoil } from '../recoil/userRecoil';
-import Login from './SignIn';
+import Header from '@components/header/Header';
+import { userReCoil } from '@recoil/userRecoil';
+import Login from '@pages/SignIn';
+import MainPage from '@pages/MainPage';
 
 export default function Layout() {
   const [loginUser] = useRecoilState(userReCoil);
+  const [pathName, setPathName] = useState('');
+
+  useEffect(() => {
+    const nowPathName = window.location.pathname;
+    setPathName(nowPathName);
+  }, []);
 
   return (
     <>
       {loginUser.jwt === '' ? (
         <Login />
+      ) : pathName === '/' ? (
+        <MainPage />
       ) : (
         <>
           <Header />
