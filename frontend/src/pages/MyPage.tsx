ce7503992054d9ad736424826a4adea09664c8b4
iamyoojincha <iamyoojincha@gmail.com>
2022-11-07 10:21:33 +0900

    :recycle: css코드 리팩토링

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -47,57 +47,21 @@ export default function MyPage() {
 }
 
 const totalCSS = css`
-  display: flex;
-  flex-direction: column;
   width: 100vw;
+  height: 100vh;
 
-  .main_tab {
-    display: flex;
-    flex-direction: row;
-  }
-  .button_tab {
-    width: 100%;
-  }
   button {
-    border-radius: 8px;
     border: 1px solid transparent;
     padding: 0.6em 1.2em;
-    font-size: 1em;
-    font-weight: 500;
-    font-family: inherit;
-    cursor: pointer;
-    transition: border-color 0.25s;
-  }
-  .btn,
-  .active_btn {
-    height: 40px;
     width: 50%;
   }
   .btn {
     float: left;
     background-color: #16453e;
-    width: 50%;
     color: white;
-    border-radius: 0%;
-
-    &:hover {
-      outline: none;
-    }
-    &:focus {
-      outline: none;
-    }
   }
   .active_btn {
     float: left;
     background-color: white;
-    width: 50%;
-    border-radius: 0%;
-
-    &:hover {
-      outline: none;
-    }
-    &:focus {
-      outline: none;
-    }
   }
 `;
