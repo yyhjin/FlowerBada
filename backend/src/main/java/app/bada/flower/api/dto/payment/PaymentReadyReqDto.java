df61b0d11aca11ab5abf3089674e364d3326b0a0
Ssoco <dlscjs8646@gmail.com>
2022-11-14 03:57:25 +0900

    :recycle: Payment 요청 관련 DTO 수정

---
+++ backend/src/main/java/app/bada/flower/api/dto/payment/PaymentReadyReqDto.java
@@ -0,0 +1,20 @@
+package app.bada.flower.api.dto.payment;
+
+import lombok.Data;
+
+@Data
+public class PaymentReadyReqDto {
+    private String optionType;
+    private int rollingId;
+    private int flowerCnt;
+    private int paginationId;
+    private String title;
+    private String userToken;
+    private int price;
+    private String receiverName;
+    private String receiverPhone;
+    private String receiverAddress;
+    private String senderName;
+    private String senderPhone;
+    private boolean isAnonymous;
+}
